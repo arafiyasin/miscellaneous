5fa001a195bedabeb63f529947d3f3ff90fd8585
arafiyasin <114900694+arafiyasin@users.noreply.github.com>
2024-10-16 14:30:18 -0700

    Update telescope_view.py

--- telescope_view.py
+++ telescope_view.py
@@ -11,11 +11,36 @@ import io
 from PIL import Image, ImageOps
 
 hide_streamlit_style = """
-            <style>
-            [data-testid="stToolbar"] {visibility: hidden !important;}
-            footer {visibility: hidden !important;}
-            </style>
-            """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
